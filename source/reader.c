443849d798030c0e1b889443f6fe3e1335c86ce1
Davis <davismarcin@gmail.com>
2023-07-02 16:31:37 +0200

    Added logger thread.

--- source/reader.c
+++ source/reader.c
@@ -5,17 +5,18 @@
 #include "../header/globals.h"
 #include "../header/reader.h"
 
-unsigned int lines_to_read()
-{ 
+unsigned int lines_to_read(){
+
   unsigned int lines = 0;
   char ch;
   FILE* fp= fopen("/proc/stat", "r");
   if(fp == NULL){
+    logger("Error with opening /proc/stat file");
     printf("Error!");   
     exit(1);             
   }
 
-  for (ch = getc(fp); ch != EOF; ch=getc(fp) ){
+  for (ch = getc(fp); ch != EOF; ch=getc(fp)){
     if (ch == '\n') 
       lines++;
   }
@@ -25,13 +26,12 @@ unsigned int lines_to_read()
   return lines;
 }
 
-void *read_data(void* thread_dataPtr)
-{ 
+void *read_data(void* thread_dataPtr){
+
   struct threads_data *thread_data = (struct threads_data*)thread_dataPtr;
   size_t k=0;
   unsigned int core_numbers =(unsigned int)thread_data->number_of_cores;
   char* array_data_lines[core_numbers];
-  //int size = sizeof(&array_data_lines);
   memset(array_data_lines,0,sizeof(array_data_lines));
   
   while(1){  
@@ -41,21 +41,24 @@ void *read_data(void* thread_dataPtr)
     for(unsigned int core =0; core<core_numbers;core++){
 
       if(getline(&array_data_lines[core], &k , fp)<0){  
-          free(array_data_lines[core]);
-          array_data_lines[core]=NULL;
+        free(array_data_lines[core]);
+        array_data_lines[core]=NULL;
       }
 
-      if(strstr(array_data_lines[core],"intr"))
+      if(strstr(array_data_lines[core],"intr")){
         break;
-write(thread_data->reader_analyzer[1],array_data_lines[core] ,70);
+      }
+
+      int result = write(thread_data->reader_analyzer[1],array_data_lines[core] ,70);
+      if(result == -1){
+        logger("Error with sending data to analyzer");
+      }
+
       free(array_data_lines[core]);   
       array_data_lines[core]=NULL;
     }
     fclose(fp);
-        thread_data->watch(0);
-
+    thread_data->watch(0);
     sem_post(&thread_data->reader_send_ready);
-
-   // sleep(1);
   } 
 }
